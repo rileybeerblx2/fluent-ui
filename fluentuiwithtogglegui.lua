eaa227fce1af490435b828f079290ef98f96e779
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-17 18:30:29 +0800

    Update fluentuiwithtogglegui.lua

--- fluentuiwithtogglegui.lua
+++ fluentuiwithtogglegui.lua
@@ -39,7 +39,6 @@ end)
 UITextSizeConstraint.Parent = TextButton
 UITextSizeConstraint.MaxTextSize = 30
 
-
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
