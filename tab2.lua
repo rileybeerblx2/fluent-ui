e2e97be6323bedf76b423915b02098d1ecedeebf
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-30 07:29:27 +0800

    Create tab2.lua

---
+++ tab2.lua
@@ -0,0 +1,8 @@
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+}
+
+local Options = Fluent.Options
+
+do
