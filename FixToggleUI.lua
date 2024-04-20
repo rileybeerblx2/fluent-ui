71a8ecff280cdcde2d683a541d1921c9b03844ec
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-04-20 11:41:08 +0800

    Create FixToggleUI.lua

---
+++ FixToggleUI.lua
@@ -0,0 +1,12 @@
+while r3thtoggleui do
+        function toggleuifix()
+        if Toggle then
+            Toggle = false
+            R3TH:toggle()
+            wait()
+        end
+    end
+    wait()
+    pcall(toggleuifix)
+    end
+end
